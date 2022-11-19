e656625cb4b8070287a810b946825b4d58a46b2c
Ivan <clicker.heroes.acg@gmail.com>
2022-11-19 16:25:44 +0300

    fix

--- scanner/src/rds.rs
+++ scanner/src/rds.rs
@@ -5,17 +5,17 @@ use redis::Commands;
 fn save_process_cache(pid: u32, name: String) -> redis::RedisResult<()> {
     let client = redis::Client::open("redis://127.0.0.1/")?;
     let mut con = client.get_connection().unwrap();
-    con.set(pid, name).expect(
-        "Caching failed"
-    )?;
+    con.set(pid, name)?;
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
     use crate::rds::save_process_cache;
-    assert_eq!(
-        save_process_cache(324123, "SomeStr"),
-        Ok(())
-    );
+    fn save_p_test() {
+        assert_eq!(
+            save_process_cache(324123, String.from("SomeStr")),
+            Ok(())
+        );
+    }
 }
