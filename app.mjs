26cf7a083f97c4acb9ab892a339bcfddc7cfb452
Ivan <clicker.heroes.acg@gmail.com>
2022-11-19 07:43:20 +0300

    refactored

---
+++ app.mjs
@@ -0,0 +1,3 @@
+import {app} from "./src/bot.mjs";
+
+app()
--- src/bot.mjs
+++ src/bot.mjs
@@ -3,38 +3,39 @@ import {config} from "dotenv";
 import {deleteFromCache, toCache} from "./cache.mjs";
 import {Observer} from "./observer.mjs";
 
-config()
-
-const bot = new Telegraf(process.env.BOT_TOKEN);
-
-bot.launch({port: process.env.PORT})
-.then(
-    () => {
-      console.log("Bot runs!")
-    }
-)
-.catch(err => console.error(err))
-
-bot.command('start', async ctx => {
-  const greeting = "Hi, i'll notify you!";
-  const id = ctx.chat.id;
-  toCache(ctx.name, id).then(() => {
-    bot.telegram.sendMessage(id, greeting).then(
-        () => console.log("Message sent to " + id)
+function commands(bot) {
+  bot.command('start', async ctx => {
+    const greeting = "Hi, i'll notify you!";
+    const id = ctx.chat.id;
+    toCache(ctx.name, id).then(() => {
+      bot.telegram.sendMessage(id, greeting).then(
+          () => console.log("Message sent to " + id)
+      )
+    })
+  })
+  bot.command('off', async ctx => {
+    const id = ctx.chat.id;
+    deleteFromCache(id).then(
+        () => {
+          bot.telegram.sendMessage(id, 'Notifications turned off.').then(
+              () => console.log('Notifications turned off for: ' + id)
+          )
+        }
     )
   })
-})
+}
 
-bot.command('off', async ctx => {
-  const id = ctx.chat.id;
-  deleteFromCache(id).then(
+export const app = () => {
+  config()
+  const bot = new Telegraf(process.env.BOT_TOKEN);
+  bot.launch({port: process.env.PORT})
+  .then(
       () => {
-        bot.telegram.sendMessage(id, 'Notifications turned off.').then(
-            () => console.log('Notifications turned off for: ' + id)
-        )
+        console.log("Bot runs!")
       }
   )
-})
-
-const obs = new Observer('../.data', bot)
-obs.watchFolder();
+  .catch(err => console.error(err))
+  commands(bot);
+  const obs = new Observer('../.data', bot)
+  obs.watchFolder();
+}
