3c5539fb76c386a55ff64a44b59f972406dd03a9
Ivan <clicker.heroes.acg@gmail.com>
2022-11-19 16:41:46 +0300

    cfg

--- scanner/src/rds.rs
+++ scanner/src/rds.rs
@@ -3,21 +3,9 @@ extern crate redis;
 use redis::Commands;
 
 pub(crate) fn save_process_cache(pid: u32, name: String) -> redis::RedisResult<()> {
-    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let url = dotenv::var("REDIS_URL").unwrap();
+    let client = redis::Client::open(String::from(url))?;
     let mut con = client.get_connection().unwrap();
     con.set(pid, name)?;
-    println!("qqqqqq");
     Ok(())
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::rds::save_process_cache;
-
-    fn test_save_process_success() {
-        assert_eq!(
-            save_process_cache(324123, String.from("SomeStr")),
-            Ok(())
-        );
-    }
 }
